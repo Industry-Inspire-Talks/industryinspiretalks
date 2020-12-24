c8fe7387df9a6e2a7e3060a82b075029b6cb6949
ganeshdubey831 <48392469+ganeshdubey831@users.noreply.github.com>
2020-12-24 21:13:14 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -13,35 +13,395 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation"></button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownId">
-                        <a class="dropdown-item" href="#">Action 1</a>
-                        <a class="dropdown-item" href="#">Action 2</a>
-                    </div>
-                </li>
+   <section id="nav-bar">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand font-weight-bold" href="#">Industry Inspire Talks</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa fa-bars hamburger"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link " href="#top">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#services">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#about-us">Our Team</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#imageslider">Our Episodes</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#design-by">Our Community</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#footer">Connect US</a>
+              </li>
             </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+             <div class="dropdown text-center bg-black menu-style ">
+          <button class="btn btn-danger dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            Menu
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="career.html">Careers</a>
+            <a class="dropdown-item" href="events.html">Events</a>
+            <a class="dropdown-item" href="blogs.html">Our Blogs</a>
+          </div>
         </div>
-    </nav>
+          </div>
+        </nav>
+      </section>
+      <!-- BANNER SECTION -->
+    <section id="banner">
+        <div class="container py-1">
+          <div class="row">
+            <div class="col-md-6 text-center justify-content-center mt-5">
+              <h2>Welcome to #inspire Community!</h2>
+              <h4>Let's get inspired!</h4>
+              <div>
+          </div>
+              </div>
+              <div class="col-md-6 text-center">
+                <img src="assets/img/undraw_experts3_3njd.svg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div>
+        </section>
+    <!-- BANNER SECTION END -->
 
-    <section id="banner"></section>
-    <section id="about_us"></section>
-    <section id="team"></section>
+<!-- About Section -->
+    <section id="about_us">
+        <div class="container">
+            <h2 class="font-weight-bold text-center mt-3 mb-5">About Our Community</h2>
+            <div class="row">
+                <div class="col-md-4">
+                <img src="assets/img/68-Customer-support.svg" alt="" class="img-fluid">
+                </div>
+                <div class="col-md-8">
+                    <h3>Industry Inspire Talks is a non-profit community founded by Omkar Shinde in August 2020.</h3><p> We believe in educating the Students/Working Professionals/Entrepreneurs by providing them guidance from the Industry Experts & helping them to make an impact in their lives by transforming themselves into an Industry-Leader. 
+It is a platform where we invite successful people from the industry to share their career journeys. We inspire people in three ways. We have three categories i.e.. Interview Series, Podcasts Series, Career Stories. 'Interview series' involves people who interned at companies & converted that internship opportunity into a full-time offer. 'Podcasts Series' involves people with 5-10 years of Industry Experience who hold big positions at companies like Founder, MD, CEO, Managers, etc. sharing their industry experiences in our series. 'Career Stories' is where we invite people who achieved immeasurable success in their careers with 10+ years of industry experience in a specific industry/company. 
+We also do Industry oriented events/ industry webinars/ workshops for our community which involves speakers from around the globe representing different industries/companies, publish inspire stories about ordinary people from the industry who have made an impact in their industry career journeys. This is what we do here, We inspire people to achieve greater heights in their career by helping them connect with successful people from the industry! Your career is our priority! 
+●   Our mission is to empower every person on this planet to achieve more in their future careers. 
+●   Our vision is to inspire people with their stories of hustle & dedication towards their dreams and make an impact in their lives! 
+●   We are here to connect you with leaders of the industry so you can transform yourself into a leader. Come let's do something for the community.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- About Section End -->
+
+    <!-- Our team S -->
+    <section id="Ourteam">
+    <!--00000000 Our Team Section  -->
+  <section id="team" class="team">
+    <div class="container text-center">
+      <header class="section-header">
+        <h3 class="mt-5 mb-5">Our Team</h3>
+      </header>
+      <div class="row  aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+        <div class="col-sm-12 col-lg-4 col-md-4 d-flex align-items-stretch">
+          <div class="member">
+            <img src="assets/img/amrita.jpeg" class="img-fluid" alt="" style="border-radius:50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Amrita Bhattacharjee </h4>
+              <span>Program Manager</span>
+              <div class="social">
+                <center>
+                  <a href="https://www.linkedin.com/in/amrita-bhattacharjee-092a171b1/"> <i class="fab fa-linkedin"></i>
+                  </a>
+                </center>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/josseph.jpg" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Joseph Nssien</h4>
+              <span>Program Manager</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/josephnssien/"><i class="fab fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/muneeb.jpg" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Muneeb Ahmed</h4>
+              <span>Community Manager</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/muneeb-ahmed-4456471a6/"><i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/ganesh.jpg" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Ganesh Dubey</h4>
+              <span>Front-End Developer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/ganesh-dubey-758694179/"><i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/omkar.jpg" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Omkar Desai</h4>
+              <span>Frontend Web Developer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/omkar-desai-472532177/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/himanshi.jpg" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Himanshi Lodha</h4>
+              <span>Program Manager</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/himanshi-lodha-47b9961a7/"><i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12 mt-4 mb-4">
+          <div class="text-center">
+            <button class="btn btn-primary" id="mbtn" data-toggle="collapse" type="button" data-target="#collapseExample" aria-expanded="false"
+              aria-controls="collapseExample"><span>See More</span>
+              <i class="fa fa-arrow-right"></i></button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Members -->
+  <div class="collapse text-center" id="collapseExample" style="overflow: hidden;margin: 20px;">
+    <div class="container-fluid aos-init aos-animate card card-body">
+      <div class="row justify-content-center mt-5">
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="" class="img-fluid" alt="" style="border-radius: 50%;width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Eshaan Chauhan</h4>
+              <span>Marketing Manager</span>
+              <div class="social">
+                <center>
+                  <a href=" https://www.linkedin.com/in/eshaan-chauhan-1a9989152/"> <i class="fab fa-linkedin"></i> </a>
+                </center>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="" class="img-fluid" alt="" style="border-radius: 50%;width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Ajinkya Sonawale</h4>
+              <span> Backend Developer</span>
+              <div class="social">
+                <center>
+                  <a href="https://www.linkedin.com/in/ajinkya-sonawale-575ba9187/"> <i class="fab fa-linkedin"></i> </a>
+                </center>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/aayush.jpg" class="img-fluid" alt="" style="border-radius: 50%;width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Aayush Vishnoi </h4>
+              <span>Front-End Developer </span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/aayush-vishnoi-2b53b01b0/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/rihen.jpg" class="img-fluid" alt="" style="border-radius: 50%; width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Rihen Vora</h4>
+              <span>Backend Developer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/vorarihen/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/karan.jpg" class="img-fluid" alt="" style="border-radius: 50%; width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Karan Sawant</h4>
+              <span>Backend Developer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/karansawant45/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/ashish.jpg" class="img-fluid" alt="" style="border-radius: 50%; width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Ashish Mohapatra</h4>
+              <span>Backend Developer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/asish-mohapatra-228180179/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/varad.jpg" class="img-fluid" alt="" style="border-radius: 50%; width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Varad Gaidhani</h4>
+              <span>Community Manager </span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/varad-gaidhani-281374171/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/ali asgar.jpg" class="img-fluid" alt="" style="border-radius: 50%;width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Ali Asgar Tinwala</h4>
+              <span>Graphic Design Head</span>
+              <div class="social">
+                <a href=" https://www.linkedin.com/in/ali-asgar-tinwala-a14165a0/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="assets/img/shashwat.jpg" class="img-fluid" alt="" style="border-radius: 50%; width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Shashwat Mulgund </h4>
+              <span>Graphic Designer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/shashwat-mulgund-644b30146/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="member">
+            <img src="" class="img-fluid" alt="" style="border-radius: 50%;width:50%; height: 50%;">
+            <div class="member-content">
+              <h4>Saurav Warekar</h4>
+              <span>Graphic Designer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/saurav-warekar-16221b1b9/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End of Members Section -->
+<div class="container text-center">
+      <header class="section-header">
+        <h3 class="mt-5 mb-5">Meet our Board of Directors</h3>
+      </header>
+      <div class="row  aos-init aos-animate" data-aos="zoom-in" data-aos-delay="100">
+        <div class="col-md-6">
+          <div class="member">
+            <img src="" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Omkar Shinde</h4>
+              <span>Founder & CEO</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/omkar-shinde-310aa5193/"><i class="fab fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col-md-6">
+          <div class="member">
+            <img src="" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Satya Prakash Mohapatra</h4>
+              <span>Chief Marketing Oﬃcer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/satya-prakash-mohapatra-153926129/"><i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="member">
+            <img src="" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Vishal Nair</h4>
+              <span>Chief People Oﬃcer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/vishalnairm/"><i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="member">
+            <img src="" class="img-fluid" alt="" style="border-radius: 50%;width:60%; height: 60%;">
+            <div class="member-content">
+              <h4>Danish Shaikh</h4>
+              <span>Chief Technology Oﬃcer</span>
+              <div class="social">
+                <a href="https://www.linkedin.com/in/shaikhdanish04/"> <i class="fab fa-linkedin"></i> </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  <!-- Board Of Directors -->
+    </section>
+     <!-- 00000000 End of Our team Section -->
+    <!-- Out Team End -->
     <section id="episode"></section>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
