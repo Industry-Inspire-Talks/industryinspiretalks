37c9007fc5662f8eb4ff5d2baf9b620bad1c726a
Tanmay Jadhav <tanmayjadhavtj11@gmail.com>
2020-12-24 15:26:46 +0530

    gitignore files removed

--- admin/admin_panel/app/Http/Controllers/User_authentication.php
+++ admin/admin_panel/app/Http/Controllers/User_authentication.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Redirect;
 use App\Models\User;
 use Auth;
 
@@ -44,21 +45,56 @@ class User_authentication extends Controller
 
     public function getLogin()
     {
-         return view('adminpages.login');
+        if (auth()->check())
+        {
+            return redirect('/adminpanel');
+        }
+        else
+        {
+            return view('adminpages.login');
+        }
+        
     }
 
     public function login(Request $request)
     {
-        $email = $request->input('Email');
-        $password = $request->input('Password');
 
-        if(Auth::attempt(['email'=>$email,'password'=>$password]))
         {
-            return redirect('/adminpanel');
+            // $email = $request->input('Email');
+            // $password = $request->input('Password');
+            // // $remember_token = true;
+
+            $email = $request->input('Email');
+            $password = $request->input('Password');
+            $remember_me = $request->input('remember_me');
+            // dd(isset($remember_me));
+            if(isset($remember_me))
+            {   
+                echo('true');
+                $remember_token = true;
+            }
+            else 
+            {
+                echo('false');
+                $remember_token = false;
+            }
+           
+
+            // // $remember_me  = $request->has('remember_me'); 
+            // // // $remember_me  = ( !empty( $request->remember_me ) )? TRUE : FALSE;
+            // // dd($remember_me);
+
+            if(Auth::attempt(['email'=>$email,'password'=>$password],$remember_token))
+            {
+                return redirect('/adminpanel');
+            }
+            else
+            {
+                return redirect()->back()->with('alert','Login Details Not Found. Please Signup First');
+            }
+            
         }
-        else{
-            return redirect()->back()->with('alert','Login Details Not Found. Please Signup First');
-        }
+
     }
 
     public function logout(Request $request) {
