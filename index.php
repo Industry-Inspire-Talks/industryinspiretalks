54c7cb9c078559241ade62305b9dd381674a2cf0
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2020-12-26 20:02:14 +0530

    favicon and title and cache clear

--- index.php
+++ index.php
@@ -5,13 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="Description" content="Enter your description here" />
+    <link rel="shortcut icon" href="assets/img/init_logo.jpg" type="image/x-icon">
+
+    <meta name="Description" content="Industry Inspire Talks" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?0800H">
     <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 
-    <title>Title</title>
+    <title>Industry Inspire Talks</title>
 </head>
 
 <body>
