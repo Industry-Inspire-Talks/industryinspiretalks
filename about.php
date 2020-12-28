6a7351b08f0b04fdb33a6626979b9d1c5004b9d1
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2020-12-28 23:03:13 +0530

    raw structure

---
+++ about.php
@@ -0,0 +1,24 @@
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+      <?php require_once('components/head.php') ?>
+
+  </head>
+
+  <body>
+      <?php require_once('components/navbar.php') ?>
+
+      <!-- Page Sections -->
+
+
+
+      <!-- Page Sections End -->
+
+      <?php require_once('components/footer.php') ?>
+
+      <?php require_once('components/foot.php') ?>
+  </body>
+
+
+  </html>
