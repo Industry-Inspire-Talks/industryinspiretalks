1bcc311d81b2a3b90fadf15e3ad55e02e0916b42
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2020-12-31 15:50:46 +0530

    cache removed

--- components/head.php
+++ components/head.php
@@ -6,7 +6,7 @@
 <meta name="Description" content="Industry Inspire Talks" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css">
-<link rel="stylesheet" href="assets/css/style.css?0800H">
+<link rel="stylesheet" href="assets/css/style.css?1530H">
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 
 <title>Industry Inspire Talks</title>
