f6f6fbb89735ac5117f08af91da08ed231a7b620
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2020-12-31 12:07:09 +0530

    staff edit and add form stucuture ready

---
+++ admin/resources/views/pages/staff/edit.blade.php
@@ -0,0 +1,52 @@
+<form action="" method="post">
+    <div class="card">
+        <div class="card-header">
+            <p class="h3">Add New Member</p>
+        </div>
+        <div class="card-body">
+            <div class="form-group">
+                <label for="">Id</label>
+                <input type="text" name="id" class="form-control" readonly>
+            </div>
+            <div class="form-group">
+                <label for="">Name</label>
+                <input type="text" name="name" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="">Designation</label>
+                <input type="text" name="designation" class="form-control" required>
+            </div>
+            <div class="form-group">
+                <label for="">Contact</label>
+                <input type="text" name="contact" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="text" name="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Date of Birth</label>
+                <input type="text" name="dob" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Nationality</label>
+                <input type="text" name="nationality" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Address</label>
+                <textarea class="form-control" name="address" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="">Date of Joining</label>
+                <input type="text" name="date_of_joining" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="">Date of Leaving</label>
+                <input type="text" name="date_of_leaving" class="form-control">
+            </div>
+        </div>
+        <div class="card-footer">
+            <button class="btn btn-success" type="submit"><i class="fa fa-plus"></i> Add</button>
+        </div>
+    </div>
+</form>
