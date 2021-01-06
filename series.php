639c375558ccd694f8f5de52c5f531570149b315
ganeshdubey <ganeshdubey831@gmail.com>
2021-01-06 19:44:48 +0530

    structure of series.php i.e Episodes page

--- series.php
+++ series.php
@@ -1,25 +1,105 @@
 <?php include('connect.php') ?>
 <!DOCTYPE html>
 <html lang="en">
+	<head>
+		<?php require_once('components/head.php') ?>
+	</head>
+	<body class="nav-nofixed">
+		<?php require_once('components/navbar.php') ?>
+		<section class="episodes">
+			<div class="container text-center">
+				<div class="row d-flex align-items-center">
+					<div class="col-md-8">
+						<h3>Our Episodes</h3>
+						<p>Watch our episodes with people from the industry & gain valuable knowledge from their industry experiences covering various industry-oriented topics! Start binge watching now!</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid" src="assets/img/events.png" alt="">
+					</div>
+				</div>
+				<!-- interview series -->
+				<div class="interview-series">
+					<div class="container">
+						<h3 class="text-center font-weight-bold mt-4 mb-5">Interview Series</h3>
+						<div class="row">
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src="..." alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Click</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src="..." alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Click</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src="..." alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Click</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- interview series End -->
 
-<head>
-    <?php require_once('components/head.php') ?>
-
-</head>
-
-<body class="nav-nofixed">
-    <?php require_once('components/navbar.php') ?>
-
-    <section class="container dummy">
-        <div class="title">Series</div>
-        <hr class="mb-4">
-
-    </section>
-    <!-- Footer Section  -->
-    <?php require_once('components/footer.php') ?>
-
-    <?php require_once('components/foot.php') ?>
-</body>
-
-
+				<!-- Podcast Series -->
+				<div class="podcast-series">
+					<div class="container">
+						<h3 class="text-center font-weight-bold mt-4 mb-5">Podcast Series</h3>
+						<div class="row">
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src="..." alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Click</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src="..." alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Click</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="card">
+									<img class="card-img-top" src="..." alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Click</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Podcast Series End -->
+			</div>
+		</section>
+		<!-- Footer Section  -->
+		<?php require_once('components/footer.php') ?>
+		<?php require_once('components/foot.php') ?>
+	</body>
 </html>
