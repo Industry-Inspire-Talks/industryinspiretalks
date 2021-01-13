7d7465c2ead739ef48df73f8751a8d5b083c43a5
Deepanjali Tripathi <68412187+Dia96@users.noreply.github.com>
2021-01-12 22:32:38 -0800

    pagenation

--- stories.php
+++ stories.php
@@ -147,13 +147,14 @@
             </div>
 
             <div class="container">
-                <ul class="pagination">
-                    <li><a href="#">1</a></li>
-                    <li><a href="#">2</a></li>
-                    <li><a href="#">3</a></li>
-                    <li><a href="#">4</a></li>
-                    <li><a href="#">5</a></li>
-                </ul>
+
+            <ul class="pagination">
+                 
+                 <li class="page-item"><a class="page-link" href="#">1</a></li>
+                 <li class="page-item"><a class="page-link" href="#">2</a></li>
+                 <li class="page-item"><a class="page-link" href="#">3</a></li>
+                 <li class="page-item"><a class="page-link" href="#">4</a></li>
+            </ul>
             </div>
 
         </body>
