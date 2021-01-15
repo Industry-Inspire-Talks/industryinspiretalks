f8db5d0ff511483ff38eda9e26f28a487d8cf303
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2021-01-15 19:17:08 +0530

    careers page ready

--- careers.php
+++ careers.php
@@ -12,9 +12,65 @@
 
     <section class="container dummy">
         <div class="title">Careers</div>
+        <p class="sub-title">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi deserunt distinctio quia aspernatur quis, suscipit illo reprehenderit numquam quasi, iste dicta? Voluptate laboriosam nobis quod ducimus. Excepturi odio ex optio.</p>
         <hr class="mb-4">
 
+        <?php if (!isset($_GET['job_id'])) { ?>
+            <div class="row">
+                <?php for ($i = 0; $i < 4; $i++) { ?>
+
+                    <div class="col-lg-4 mb-4">
+                        <div class="card shadow" style="border-radius: 1rem;height:100%">
+                            <div class="card-body d-flex flex-column align-items-between justify-content-between"">
+                                <div>
+
+                                    <p class=" h3">Job Title</p>
+                                <hr>
+                                <p class="small">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima placeat non odio voluptas assumenda illo nulla nisi exercitationem eius beatae ipsum suscipit, a fugit. Maiores distinctio earum suscipit fugit quam!</p>
+
+                                <p class="small"><b>Skills :</b> HTML, CSS, Bootstrap, PHP</p>
+
+                                <p class="small"><b>Perks :</b> Paid Vacation, Corporate Discounts, Tuition Reimbursement and more</p>
+
+                                <p class="small"><b>Tenure :</b> 6 Months | 3 Months | Project Based</p>
+                            </div>
+                            <div>
+
+                                <hr>
+                                <form action="" method="post" enctype="multipart/form-data">
+                                    <label class="small mb-1 ml-2 font-weight-bold" for="">Upload CV / Resume</label>
+                                    <div class="d-flex">
+                                        <input class="form-control p-1 mr-1" type="file" name="resume">
+                                        <button name="" class="text-nowrap btn btn-success btn-sm"><i class="fa fa-paper-plane"></i> Send</button>
+                                    </div>
+                                    <small class="text-muted">*Only <b>PDF</b> File Format Allowed.</small>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        <?php } ?>
+
+        </div>
+    <?php } else { ?>
+        <div class="card mx-auto" style="max-width:600px">
+            <div class="card-body">
+                <p class="h3">Job Title</p>
+                <hr>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima placeat non odio voluptas assumenda illo nulla nisi exercitationem eius beatae ipsum suscipit, a fugit. Maiores distinctio earum suscipit fugit quam!</p>
+
+                <p class="h6">Skills</p>
+                <ul>
+                    <li>Skill 1</li>
+                    <li>Skill 2</li>
+                    <li>Skill 3</li>
+                    <li>Skill 4</li>
+                </ul>
+            </div>
+        </div>
+    <?php } ?>
     </section>
+
     <!-- Footer Section  -->
     <?php require_once('components/footer.php') ?>
 
