686e88fffd22f3e2c55a71263d37487f917a63dd
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2021-01-23 04:19:11 +0530

    navbar fixed

--- components/navbar.php
+++ components/navbar.php
@@ -1,9 +1,10 @@
 <nav class="navbar navbar-expand-lg">
     <a class="navbar-brand" href="./index.php">
-        <img src="assets/img/init_logo.jpg" alt="" class="logo">
-        <div>
-            <p class="mb-2">Industry Inspire</p>
-            <p class="m-0">Talks</p>
+        <div class="holder">
+            <div class="d-flex align-items-center">
+                <img src="assets/img/init_logo.jpg" height="40px" alt="" class="logo">
+                <p class="m-0">Industry Inspire Talks</p>
+            </div>
         </div>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
@@ -23,15 +24,24 @@
             <li class="nav-item">
                 <a class="nav-link" href="./episodes.php">Episodes</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./episodes.php">Stories</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./episodes.php">Careers</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./episodes.php">Contacts Us</a>
+            </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle no-arrow " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     More
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="./stories.php">Inspire Stories</a>
+                    <!-- <a class="dropdown-item" href="./stories.php">Inspire Stories</a> -->
                     <a class="dropdown-item" href="./ambassador.php">Campus Ambassador Program</a>
-                    <a class="dropdown-item" href="./careers.php">Careers</a>
-                    <a class="dropdown-item" href="./contact.php">Contact Us</a>
+                    <!-- <a class="dropdown-item" href="./careers.php">Careers</a> -->
+                    <!-- <a class="dropdown-item" href="./contact.php">Contact Us</a> -->
                 </div>
             </li>
         </ul>
