5ad0d31acfd308401daa5d642cdcfdc5776769be
ShaikhDanish04 <shaikh.danish4444@gmail.com>
2021-01-23 00:58:52 +0530

    fix

--- episodes.php
+++ episodes.php
@@ -40,25 +40,25 @@
 			<div class="row align-items-center justify-content-center">
 				<div class="col-md-6 col-lg-4">
 					<a class="btn w-100" href="series.php">
-						<div class="card text-center">
-							<div class="card-img-top" style="height:240px;background:#f1f1f1">
+						<div class="card text-center" style="width: 290px;">
+							<div class="image-center-cover" style="height:290px;background-image:url('./assets/img/interview.jpeg')">
 							</div>
-							<div class="card-body">
+							<!-- <div class="card-body">
 								<p class=" h1 font-weight-normal">Interview</p>
 								<p class="m-0 h2 font-weight-light">Series</p>
-							</div>
+							</div> -->
 						</div>
 					</a>
 				</div>
 				<div class="col-md-6 col-lg-4">
 					<a class="btn w-100" href="series.php">
-						<div class="card text-center">
-							<div class="card-img-top" style="height:240px;background:#f1f1f1">
+						<div class="card text-center" style="width: 290px;">
+							<div class="image-center-cover" style="height:290px;background-image:url('./assets/img/podcast.jpeg')">
 							</div>
-							<div class="card-body">
+							<!-- <div class="card-body">
 								<p class=" h1 font-weight-normal">Podcast</p>
 								<p class="m-0 h2 font-weight-light">Series</p>
-							</div>
+							</div> -->
 						</div>
 					</a>
 				</div>
