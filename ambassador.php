6fe209c7e851abb7433ae4e85e528b843baf7955
ganeshdubey <ganeshdubey831@gmail.com>
2021-02-27 22:30:21 +0530

    structure of  Campus Ambassador Page

--- ambassador.php
+++ ambassador.php
@@ -14,9 +14,25 @@
         <?php require_once('components/navbar.php') ?>
 
         <section class="container dummy">
-            <div class="title">Campus Ambassador Program</div>
-            <hr class="mb-4">
-
+            <div class="container-fluid text-center">
+            	<h3>Why become our Campus Ambassador?</h3>
+            	<h6>description</h6>
+            </div>
+            <div class="container-fluid text-center">
+            	<h3>Perks of becoming Ambassador</h3>
+            	<div class="row">
+            		<div class="col-md-4"></div>
+            		<div class="col-md-4"></div>
+            		<div class="col-md-4"></div>
+            	</div>
+            	<div>
+            		<h3>What do we expect from you</h3>
+            		<div class="row">
+            		<div class="col-md-6"></div>
+            		<div class="col-md-6"></div>
+            	</div>
+            	</div>
+            </div>
         </section>
         <!-- Footer Section  -->
         <?php require_once('components/footer.php') ?>
