1cb62ee1ce01c7772913eb8144d2433841fb358b
Shaikh Danish <32537559+ShaikhDanish04@users.noreply.github.com>
2021-07-12 00:34:50 +0530

    done with base ui

---
+++ industryinspiretalks/resources/views/includes/footer.blade.php
@@ -0,0 +1,61 @@
+<footer id="footer">
+    <div class="footer-top">
+        <div class="row">
+            <div class="col-10 col-sm-10 col-lg-3 col-md-6">
+                <div class="footer-info">
+                    <h5>Industry Inspire Talks</h5>
+                    <p>
+                        9/2 Dhanashri CHS,
+                        Nanda Patkar Road,
+                        Vile Parle(E),
+                        Mumbai 400057, India<br><br>
+                        <strong>Phone:</strong> <br /> +91 8850810515<br>
+                        <strong>Email:</strong> industryinspiretalks2020@gmail.com<br>
+                    </p>
+                </div>
+            </div>
+
+            <div class="col-10 col-sm-10 col-lg-2 col-md-6 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                    <li> <a href="./index.php">Home</a></li>
+                    <li> <a href="./about.php">About us</a></li>
+                    <li> <a href="./community.php">Community</a></li>
+                    <li> <a href="./episodes.php">Episodes</a></li>
+                    <li> <a href="./events.php">Events</a></li>
+                    <li> <a href="./contact.php">Contact Us</a></li>
+                </ul>
+            </div>
+
+            <div class="col-10 col-sm-10 col-lg-3 col-md-6 footer-links">
+                <!-- <h4>Our Services</h4> -->
+                <ul class="mt-5">
+                    <li><a href="./stories.php">Inspire Stories </a></li>
+                    <li><a href="#">Campus Ambassador Program</a></li>
+                    <li><a href="./careers.php">Careers </a></li>
+                    <li><a href="https://www.youtube.com/channel/UCSA7txiQWbCNhkfUq37qx7A" target="blank">Our Channel</a></li>
+
+            </div>
+
+            <div class="col-12 col-sm-12 col-lg-4 col-md-6 footer-newsletter">
+                <div>
+                    <h4>Our Newsletter</h4>
+                    <p class="small">Feel free to drop your email here So you will be updated with US</p>
+                </div>
+                <form action="" method="post">
+                    <form class="form-inline">
+                        <div class="d-flex">
+                            <input type="email" name="" id="" class="form-control mr-2" placeholder="" aria-describedby="helpId">
+                            <button class="btn btn-dark"> Subscribe</button>
+                        </div>
+                    </form>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="copyright">
+        &copy; Copyright <strong><span>Industry Inspire Talks</span></strong>. All Rights Reserved
+    </div>
+
+</footer>
---
+++ industryinspiretalks/resources/views/includes/navbar.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand" href="./index.php">
+        <div class="holder">
+            <div class="d-flex align-items-center">
+                <img src="assets/img/init_logo.jpg" height="40px" alt="" class="logo">
+                <p class="m-0">Industry Inspire Talks</p>
+            </div>
+        </div>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+        <i class="fa fa-bars"></i>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="./index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./about.php">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./community.php">Community</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./events.php">Events</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./episodes.php">Episodes</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./stories.php">Inspire Stories</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle no-arrow " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    More
+                </a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="./inspire_class.php">Inspire Class</a>
+                    <a class="dropdown-item" href="./ambassador.php">Campus Ambassador Program</a>
+                    <a class="dropdown-item" href="./careers.php">Careers</a>
+                    <a class="dropdown-item" href="./contact.php">Contact Us</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
