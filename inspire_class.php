5f85f7ad5dc18bd4477db64cb360ec7bdc0202e9
Rutujarrajhans <59066365+Rutujarrajhans@users.noreply.github.com>
2021-07-11 19:39:06 +0530

    inspire class page created

---
+++ inspire_class.php
@@ -0,0 +1,27 @@
+<?php include('connect.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php require_once('components/head.php') ?>
+
+</head>
+
+<body class="nav-nofixed">
+	<?php require_once('components/preloader.php') ?>
+
+	<main>
+		<?php require_once('components/navbar.php') ?>
+
+
+
+		<!-- Footer Section  -->
+		<?php require_once('components/footer.php') ?>
+
+		<?php require_once('components/foot.php') ?>
+	</main>
+
+</body>
+
+
+</html>
