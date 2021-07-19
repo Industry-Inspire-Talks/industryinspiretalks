8da0d089106067b03308ecf32ea31dfe453eecae
Shaikh Danish <32537559+ShaikhDanish04@users.noreply.github.com>
2021-07-19 11:57:44 +0530

    under maintainance ready

---
+++ industryinspiretalks/resources/views/errors/503.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('bodyClass', 'nav-nofixed')
+
+@section('content')
+
+    <div class="d-flex align-items-center justify-content-center" style="min-height: 80vh">
+
+        <div class="text-center">
+
+            <img height="200px" class="mb-4" src="https://i.pinimg.com/originals/23/1b/02/231b02e5036ee851fc31c00f3299653a.png" alt="">
+            <p class="display-4">Under Maintainance</p>
+            <p class="">We will be live soon</p>
+        </div>
+    </div>
+@endsection
